ee67db19a0a54b4fdfad348d8d522524ae16c443
zhaoyong <xiaozhao32@hotmail.com>
2021-11-03 17:35:28 +0800

    修改获取rabbitmq账号方式

--- src/main/java/com/oner365/monitor/controller/rabbitmq/RabbitmqController.java
+++ src/main/java/com/oner365/monitor/controller/rabbitmq/RabbitmqController.java
@@ -19,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.alibaba.fastjson.JSONObject;
 import com.oner365.controller.BaseController;
+import com.oner365.util.Base64Utils;
 import com.oner365.util.DataUtils;
 
 import io.swagger.annotations.Api;
@@ -38,8 +39,12 @@ public class RabbitmqController extends BaseController {
 
     @Value("${spring.rabbitmq.host}")
     private String host;
-
-    private static final String AUTH = "Basic YWRtaW46YWRtaW4xMjM=";
+    
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     /**
      * 首页
@@ -50,7 +55,7 @@ public class RabbitmqController extends BaseController {
     @ApiOperation("首页")
     public JSONObject index() {
         String uri = "/api/overview";
-        Mono<JSONObject> mono = getWebClient().get().uri(uri).header(HttpHeaders.AUTHORIZATION, AUTH).retrieve()
+        Mono<JSONObject> mono = getWebClient().get().uri(uri).header(HttpHeaders.AUTHORIZATION, getAuthorization()).retrieve()
                 .bodyToMono(JSONObject.class);
         return mono.block();
     }
@@ -64,6 +69,11 @@ public class RabbitmqController extends BaseController {
     private String getUrl(String paramName, String name, int pageIndex, int pageSize) {
         return "/api/" + paramName + "?page=" + pageIndex + "&page_size=" + pageSize + "&name="
                 + DataUtils.trimToEmpty(name) + "&use_regex=false&pagination=true";
+    }
+    
+    private String getAuthorization() {
+        String auth = username + ":" + password;
+        return "Basic " + Base64Utils.encodeBase64String(auth.getBytes());
     }
 
     /**
@@ -102,7 +112,7 @@ public class RabbitmqController extends BaseController {
             paramJson.put("mode", "delete");
             paramJson.put("name", name);
             Mono<JSONObject> mono = getWebClient().method(HttpMethod.DELETE).uri(uri)
-                    .header(HttpHeaders.AUTHORIZATION, AUTH).body(BodyInserters.fromValue(paramJson)).retrieve()
+                    .header(HttpHeaders.AUTHORIZATION, getAuthorization()).body(BodyInserters.fromValue(paramJson)).retrieve()
                     .bodyToMono(JSONObject.class);
             
             return mono.block();
@@ -113,7 +123,7 @@ public class RabbitmqController extends BaseController {
     }
 
     private JSONObject request(String uri) {
-        Mono<JSONObject> mono = getWebClient().get().uri(uri).header(HttpHeaders.AUTHORIZATION, AUTH).retrieve()
+        Mono<JSONObject> mono = getWebClient().get().uri(uri).header(HttpHeaders.AUTHORIZATION, getAuthorization()).retrieve()
                 .bodyToMono(JSONObject.class);
         return mono.block();
     }
