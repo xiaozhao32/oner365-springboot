d0894eb1a9461bb9969e359133bad8877381ac21
刘涛 <liutao@nationsky.com>
2022-01-12 15:46:18 +0800

    1、添加webclient跳过ssl验证 2、添加webclient跳过ssl验证开关

--- src/main/java/com/oner365/common/config/WebClientConfig.java
+++ src/main/java/com/oner365/common/config/WebClientConfig.java
@@ -1,10 +1,20 @@
 package com.oner365.common.config;
 
+import javax.net.ssl.SSLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
 
 /**
  * WebClient Config
@@ -14,10 +24,34 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @Configuration
 public class WebClientConfig {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebClientConfig.class);
+	
+    /**
+     * 是否ssl验证开关
+     */
+    @Value("${webclient.ssl.vaild}")
+    private boolean sslVaild;
 
     @Bean
     public WebClient webClient() {
     	ClientHttpConnector httpConnector = new ReactorClientHttpConnector();
+    	if(!sslVaild) {
+			SslContext sslContext = null;
+		    try {
+		        sslContext = SslContextBuilder
+		                .forClient()
+		                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+		                .build();
+			} catch (SSLException e) {
+				LOGGER.error("ssl exception:{}",e);
+			}
+		
+		    SslContext finalSslContext = sslContext;
+		    HttpClient httpClient = HttpClient.create()
+		            .secure(sslSpec -> sslSpec.sslContext(finalSslContext));
+			httpConnector = new ReactorClientHttpConnector(httpClient);
+    	}
 		return WebClient.builder().clientConnector(httpConnector).build();
     }
 
