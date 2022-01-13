c0234a696994461750c92b83f63cc953b31c9d23
刘涛 <liutao@nationsky.com>
2022-01-13 12:49:01 +0800

    优化

--- src/test/java/com/oner365/test/util/JwtUtilsTest.java
+++ src/test/java/com/oner365/test/util/JwtUtilsTest.java
@@ -28,9 +28,9 @@ class JwtUtilsTest extends BaseUtilsTest {
     	String token = JwtUtils.generateToken(json.toJSONString(), time, key);
     	LOGGER.info("token:{}",token);
     	LOGGER.info("解密token:{}",JwtUtils.getUsernameFromToken(token, key));
-    	LOGGER.info("validateToken:",JwtUtils.validateToken(token, key));
+    	LOGGER.info("validateToken:{}",JwtUtils.validateToken(token, key));
     	LOGGER.info("base64 decode:");
-    	Arrays.asList(token.split("\\.")).stream().filter(s-> s.startsWith("ey")).forEach(s -> System.out.println(new String(Base64.getDecoder().decode(s.trim()))));
+    	Arrays.asList(token.split("\\.")).stream().filter(s-> s.startsWith("ey")).forEach(s -> LOGGER.info("content:{}",new String(Base64.getDecoder().decode(s.trim()))));
     	
     }
 }
