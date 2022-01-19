b12b552680d460f9892e9673d0166481dc84aaa6
zhaoyong <xiaozhao32@hotmail.com>
2022-01-19 12:43:15 +0800

    优化配置文件

--- src/main/java/com/oner365/generator/config/GenConfig.java
+++ src/main/java/com/oner365/generator/config/GenConfig.java
@@ -1,34 +1,29 @@
 package com.oner365.generator.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 /**
  * 读取代码生成相关配置
  * 
  * @author zhaoyong
  */
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "gen")
 @PropertySource(value = { "classpath:generator.yml" })
 public class GenConfig {
     
     /** 作者 */
-    @Value("${author}")
     private String author;
 
     /** 生成包路径 */
-    @Value("${packageName}")
     private String packageName;
 
     /** 自动去除表前缀，默认是false */
-    @Value("${autoRemovePre}")
     private boolean autoRemovePre;
 
     /** 表前缀(类名不会包含表前缀) */
-    @Value("${tablePrefix}")
     private String tablePrefix;
 
     public GenConfig() {
