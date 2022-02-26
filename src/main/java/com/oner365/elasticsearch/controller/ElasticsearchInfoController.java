fe3209528302cc0a1356766efac40eb626f00f5b
zhaoyong <xiaozhao32@hotmail.com>
2022-02-26 22:58:43 +0800

    修改 elasticsearch 过期类

--- src/main/java/com/oner365/elasticsearch/controller/ElasticsearchInfoController.java
+++ src/main/java/com/oner365/elasticsearch/controller/ElasticsearchInfoController.java
@@ -1,27 +1,36 @@
 package com.oner365.elasticsearch.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.GetAliasesResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.cluster.metadata.AliasMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.oner365.common.config.properties.CommonProperties;
 import com.oner365.controller.BaseController;
 import com.oner365.elasticsearch.dto.ClusterDto;
 import com.oner365.elasticsearch.dto.TransportClientDto;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Elasticsearch 信息
@@ -31,7 +40,6 @@ import java.util.List;
  */
 @RestController
 @Api(tags = "Elasticsearch 信息")
-@SuppressWarnings({ "deprecation", "resource" })
 @RequestMapping("/elasticsearch/info")
 public class ElasticsearchInfoController extends BaseController {
 
@@ -47,36 +55,37 @@ public class ElasticsearchInfoController extends BaseController {
   @ApiOperationSupport(order = 1)
   @GetMapping("/index")
   public TransportClientDto index() {
-    String hostname = StringUtils.substringBefore(commonProperties.getUris(), ":");
-    int port = 9300;
-    // 指定集群
-    Settings settings = Settings.builder().build();
     // 创建客户端
-    try (final TransportClient client = new PreBuiltTransportClient(settings)
-        .addTransportAddress(new TransportAddress(InetAddress.getByName(hostname), port))) {
-      ClusterHealthResponse response = client.admin().cluster().prepareHealth().get();
+    ClientConfiguration configuration = ClientConfiguration.builder().connectedTo(commonProperties.getUris()).build();
+    try (RestHighLevelClient client = RestClients.create(configuration).rest()) {
+      ClusterHealthResponse healthResponse = client.cluster().health(new ClusterHealthRequest(), RequestOptions.DEFAULT);
 
       TransportClientDto result = new TransportClientDto();
-      result.setHostname(hostname);
-      result.setPort(port);
-      result.setClusterName(response.getClusterName());
-      result.setNumberOfDataNodes(response.getNumberOfDataNodes());
-      result.setActiveShards(response.getActiveShards());
-      result.setStatus(response.getStatus().name());
-      result.setTaskMaxWaitingTime(response.getTaskMaxWaitingTime().getMillis());
+      result.setHostname(StringUtils.substringBefore(commonProperties.getUris(), ":"));
+      result.setPort(Integer.parseInt(StringUtils.substringAfter(commonProperties.getUris(), ":")));
+      result.setClusterName(healthResponse.getClusterName());
+      result.setNumberOfDataNodes(healthResponse.getNumberOfDataNodes());
+      result.setActiveShards(healthResponse.getActiveShards());
+      result.setStatus(healthResponse.getStatus().name());
+      result.setTaskMaxWaitingTime(healthResponse.getTaskMaxWaitingTime().getMillis());
+      
       // 索引信息
       List<ClusterDto> clusterList = new ArrayList<>();
-      response.getIndices().values().forEach(health -> {
+      GetAliasesResponse aliasResponse = client.indices().getAlias(new GetAliasesRequest(), RequestOptions.DEFAULT);
+      Map<String, Set<AliasMetadata>> aliasMap = aliasResponse.getAliases();
+      for (Entry<String, Set<AliasMetadata>> entry : aliasMap.entrySet()) {
+        SearchResponse search = client.search(new SearchRequest(entry.getKey()), RequestOptions.DEFAULT);
+
         ClusterDto clusterDto = new ClusterDto();
-        clusterDto.setIndex(health.getIndex());
-        clusterDto.setNumberOfShards(health.getNumberOfShards());
-        clusterDto.setNumberOfReplicas(health.getNumberOfReplicas());
-        clusterDto.setStatus(health.getStatus().toString());
+        clusterDto.setIndex(entry.getKey());
+        clusterDto.setNumberOfShards(search.getTotalShards());
+        clusterDto.setNumberOfReplicas(search.getNumReducePhases());
+        clusterDto.setStatus(search.status().name());
         clusterList.add(clusterDto);
-      });
+      }
       result.setClusterList(clusterList);
       return result;
-    } catch (UnknownHostException e) {
+    } catch (Exception e) {
       logger.error("index error:", e);
     }
     return null;
