86729ab179bf6bc3472e3a95053d5aeb2d6d2ab2
刘涛 <liutao@nationsky.com>
2022-03-01 15:44:46 +0800

    1、修改启动shell，增加jvm优化配置 2、修改配置文件增加tomcat连接数配置

--- src/main/resources/service/start.sh
+++ src/main/resources/service/start.sh
@@ -31,6 +31,9 @@ else
 fi
  
 rm -f tpid
+#4核8G配置jvm(根据服务器做调整，翻倍即可)
+#nohup java  -Xms4g -Xmx4g -Xmn2g -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -XX:MaxDirectMemorySize=1g -XX:+PrintGCDetails  -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -Xloggc:../logs/gc.log -jar -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=../logs/dump.hprof -Dspring.profiles.active=$ACTIVE ./$RESOURCE_NAME > ../logs/$RESOURCE_NAME.log 2>&1 &
+#默认启动jvm不调优
 nohup java -jar -Dspring.profiles.active=$ACTIVE ./$RESOURCE_NAME > ../logs/$RESOURCE_NAME.log 2>&1 &
 
 echo $! > tpid
