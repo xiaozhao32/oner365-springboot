46ffb0ef355ea940eacc74a37216a9a70970fbae
zhaoyong <xiaozhao32@hotmail.com>
2022-05-16 11:55:04 +0800

    修改单元测试

--- src/test/java/com/oner365/test/service/common/PropertiesTest.java
+++ src/test/java/com/oner365/test/service/common/PropertiesTest.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.alibaba.fastjson.JSON;
-import com.oner365.files.config.properties.FileFdfsProperties;
-import com.oner365.files.config.properties.FileLocalProperties;
-import com.oner365.files.config.properties.MinioProperties;
+import com.oner365.common.config.properties.AccessTokenProperties;
 import com.oner365.test.service.BaseServiceTest;
 
 /**
@@ -20,29 +18,11 @@ import com.oner365.test.service.BaseServiceTest;
 class PropertiesTest extends BaseServiceTest {
 
   @Autowired
-  private FileLocalProperties fileLocalProperties;
-  
-  @Autowired
-  private FileFdfsProperties fileFdfsProperties;
-  
-  @Autowired
-  private MinioProperties minioProperties;
+  private AccessTokenProperties properties;
   
   @Test
-  void fileLocalPropertiesTest() {
-    Assertions.assertNotNull(fileLocalProperties);
-    logger.info("Local properties:{}", JSON.toJSONString(fileLocalProperties));
-  }
-  
-  @Test
-  void fileFdfsPropertiesTest() {
-    Assertions.assertNotNull(fileFdfsProperties);
-    logger.info("Fdfs properties:{}", JSON.toJSONString(fileFdfsProperties));
-  }
-  
-  @Test
-  void fileMinioPropertiesTest() {
-    Assertions.assertNotNull(minioProperties);
-    logger.info("Minio properties:{}", JSON.toJSONString(minioProperties));
+  void filePropertiesTest() {
+    Assertions.assertNotNull(properties);
+    logger.info("properties:{}", JSON.toJSONString(properties));
   }
 }
