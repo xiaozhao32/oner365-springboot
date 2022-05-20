b6a309f49029b5925d9aef0a1b5401255aaedb7d
zhaoyong <xiaozhao32@hotmail.com>
2022-05-20 14:52:26 +0800

    添加生成po的json类型

--- src/main/java/com/oner365/generator/util/GenUtils.java
+++ src/main/java/com/oner365/generator/util/GenUtils.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.oner365.common.constants.PublicConstants;
 import com.oner365.generator.config.GenConfig;
 import com.oner365.generator.constants.GenConstants;
 import com.oner365.generator.entity.GenTable;
@@ -21,47 +22,54 @@ public class GenUtils {
   /**
    * 数据库字符串类型
    */
-  protected static final String[] COLUMN_TYPE_STR = {"char", "varchar", "nvarchar", "varchar2", "tinytext", "text",
-          "mediumtext", "longtext"};
+  protected static final String[] COLUMN_TYPE_STR = { "char", "varchar", "nvarchar", "varchar2", "tinytext", "text",
+      "mediumtext", "longtext" };
 
   /**
    * 数据库时间类型
    */
-  protected static final String[] COLUMN_TYPE_DATE_TIME = {GenConstants.HTML_DATE_TIME, "time", "timestamp"};
+  protected static final String[] COLUMN_TYPE_DATE_TIME = { GenConstants.HTML_DATE_TIME, "time", "timestamp" };
 
   /**
    * 数据库日期类型
    */
-  protected static final String[] COLUMN_TYPE_DATE = {GenConstants.HTML_DATE};
+  protected static final String[] COLUMN_TYPE_DATE = { GenConstants.HTML_DATE };
+  
+  /**
+   * 数据库JSON类型
+   */
+  protected static final String[] COLUMN_TYPE_JSON = { GenConstants.TYPE_JSON };
 
   /**
    * 数据库数字类型
    */
-  protected static final String[] COLUMN_TYPE_NUMBER = {"tinyint", "smallint", "mediumint", "int", "number",
-          "integer", "bit", "bigint", "float", "double", "decimal"};
+  protected static final String[] COLUMN_TYPE_NUMBER = { "tinyint", "smallint", "mediumint", "int", "number", "integer",
+      "bit", "bigint", "float", "double", "decimal" };
 
   /**
    * 页面不需要编辑字段
    */
-  protected static final String[] COLUMN_NAME_NOT_EDIT = {"id", GenConstants.PARAM_CREATE_USER,
-          GenConstants.PARAM_CREATE_TIME};
+  protected static final String[] COLUMN_NAME_NOT_EDIT = { GenConstants.PARAM_ID, GenConstants.PARAM_CREATE_USER,
+      GenConstants.PARAM_CREATE_TIME };
 
   /**
    * 页面不需要显示的列表字段
    */
-  protected static final String[] COLUMN_NAME_NOT_LIST = {"id", GenConstants.PARAM_CREATE_USER,
-          GenConstants.PARAM_CREATE_TIME, "update_user"};
+  protected static final String[] COLUMN_NAME_NOT_LIST = { GenConstants.PARAM_ID, GenConstants.PARAM_CREATE_USER,
+      GenConstants.PARAM_CREATE_TIME, GenConstants.PARAM_UPDATE_USER };
 
   /**
    * 页面不需要查询字段
    */
-  protected static final String[] COLUMN_NAME_NOT_QUERY = {"id", GenConstants.PARAM_CREATE_USER,
-          GenConstants.PARAM_CREATE_TIME, "update_user", "update_time", "remark"};
+  protected static final String[] COLUMN_NAME_NOT_QUERY = { GenConstants.PARAM_ID, GenConstants.PARAM_CREATE_USER,
+      GenConstants.PARAM_CREATE_TIME, GenConstants.PARAM_UPDATE_USER, GenConstants.PARAM_UPDATE_TIME,
+      GenConstants.PARAM_REMARK };
 
   private static final int POS_2 = 2;
   private static final int POS_10 = 10;
 
   public static final String PARAM_NAME = "name";
+  public static final String PARAM_SEX = "sex";
   public static final String PARAM_STATUS = "status";
   public static final String PARAM_TYPE = "type";
   public static final String PARAM_IMAGE = "image";
@@ -81,7 +89,8 @@ public class GenUtils {
    * 初始化表信息
    */
   public static void initTable(GenConfig genConfig, GenTable genTable, String operName) {
-    genTable.setClassName(convertClassName(genTable.getTableName(), genConfig.getTablePrefix(), genConfig.getAutoRemovePre()));
+    genTable.setClassName(
+        convertClassName(genTable.getTableName(), genConfig.getTablePrefix(), genConfig.getAutoRemovePre()));
     genTable.setPackageName(genConfig.getPackageName());
     genTable.setModuleName(getModuleName(genConfig.getPackageName()));
     genTable.setBusinessName(getBusinessName(genTable.getTableName()));
@@ -113,6 +122,9 @@ public class GenUtils {
     } else if (arraysContains(COLUMN_TYPE_DATE_TIME, dataType)) {
       column.setJavaType(GenConstants.TYPE_DATE_TIME);
       column.setHtmlType(GenConstants.HTML_DATE_TIME);
+    } else if (arraysContains(COLUMN_TYPE_JSON, dataType)) {
+      column.setJavaType(GenConstants.TYPE_JSON);
+      column.setHtmlType(GenConstants.HTML_TEXTAREA);
     } else if (arraysContains(COLUMN_TYPE_NUMBER, dataType)) {
       column.setHtmlType(GenConstants.HTML_INPUT);
 
@@ -158,7 +170,7 @@ public class GenUtils {
     }
     // 类型&性别字段设置下拉框
     else if (StringUtils.endsWithIgnoreCase(columnName, PARAM_TYPE)
-            || StringUtils.endsWithIgnoreCase(columnName, "sex")) {
+        || StringUtils.endsWithIgnoreCase(columnName, PARAM_SEX)) {
       column.setHtmlType(GenConstants.HTML_SELECT);
     }
     // 文件字段设置上传控件
@@ -228,7 +240,8 @@ public class GenUtils {
    * @return String
    */
   public static String replaceFirst(String replacement, String[] searchList) {
-    return Arrays.stream(searchList).filter(replacement::startsWith).findFirst().map(searchString -> replacement.replaceFirst(searchString, "")).orElse(replacement);
+    return Arrays.stream(searchList).filter(replacement::startsWith).findFirst()
+        .map(searchString -> replacement.replaceFirst(searchString, "")).orElse(replacement);
   }
 
   /**
@@ -238,7 +251,7 @@ public class GenUtils {
    * @return 替换后的名字
    */
   public static String replaceText(String text) {
-    return RegExUtils.replaceAll(text, "(?:表|Oner365)", "");
+    return RegExUtils.replaceAll(text, "(?:表|" + PublicConstants.NAME + ")", "");
   }
 
   /**
