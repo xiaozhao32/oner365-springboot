61c8581c360ba6039abaf5a420ee70b72848bbc9
刘涛 <liutao@cncytech.com>
2022-06-24 15:30:51 +0800

    格式化

--- src/main/java/com/oner365/common/cache/config/RedisCacheConfig.java
+++ src/main/java/com/oner365/common/cache/config/RedisCacheConfig.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import java.util.Arrays;
 
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -42,6 +41,7 @@ import redis.clients.jedis.Jedis;
 
 /**
  * Redis Cache Config
+ * 
  * @author zhaoyong
  *
  */
@@ -52,71 +52,70 @@ import redis.clients.jedis.Jedis;
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisCacheConfig extends CachingConfigurerSupport {
 
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager.builder(connectionFactory)
-                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(PublicConstants.EXPIRE_TIME)))
-                .transactionAware().build();
-    }
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(
+            RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(PublicConstants.EXPIRE_TIME)))
+        .transactionAware().build();
+  }
 
-    @Bean
-    public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
+  @Bean
+  public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
+    redisTemplate.setValueSerializer(serializer);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(serializer);
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 
-        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
+    redisTemplate.afterPropertiesSet();
+    return redisTemplate;
+  }
 
-        redisTemplate.setValueSerializer(serializer);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(serializer);
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+  @Bean
+  @ConditionalOnProperty(value = { "spring.redis.cluster.enable" }, havingValue = "true")
+  public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+    RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(
+        redisProperties.getCluster().getNodes());
+    ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+        .enablePeriodicRefresh().enableAllAdaptiveRefreshTriggers().refreshPeriod(Duration.ofSeconds(5L)).build();
+    ClusterClientOptions clusterClientOptions = ClusterClientOptions.builder()
+        .topologyRefreshOptions(clusterTopologyRefreshOptions).build();
+    LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
+        .readFrom(ReadFrom.REPLICA_PREFERRED).clientOptions(clusterClientOptions).build();
+    return new LettuceConnectionFactory(redisClusterConfiguration, lettuceClientConfiguration);
+  }
 
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-    @Bean
-    @ConditionalOnProperty(value = { "spring.redis.cluster.enable" }, havingValue = "true")
-    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
-        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(redisProperties.getCluster().getNodes());
-        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
-                .enablePeriodicRefresh().enableAllAdaptiveRefreshTriggers()
-                .refreshPeriod(Duration.ofSeconds(5L)).build();
-        ClusterClientOptions clusterClientOptions = ClusterClientOptions.builder()
-                .topologyRefreshOptions(clusterTopologyRefreshOptions).build();
-        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
-                .readFrom(ReadFrom.REPLICA_PREFERRED).clientOptions(clusterClientOptions).build();
-        return new LettuceConnectionFactory(redisClusterConfiguration, lettuceClientConfiguration);
-    }
-
-    @Bean
-    @Override
-    public KeyGenerator keyGenerator() {
-        return (target, method, params) -> {
-            char sp = ':';
-            StringBuilder strBuilder = new StringBuilder(30);
-            // 类名
-            strBuilder.append(target.getClass().getSimpleName());
-            strBuilder.append(sp);
-            // 方法名
-            strBuilder.append(method.getName());
-            strBuilder.append(sp);
-            if (params.length > 0) {
-                // 参数值
-                Arrays.stream(params).forEach(object -> {
-                    if (DataUtils.isEmpty(object)) {
-                        strBuilder.append(Strings.EMPTY);
-                    } else if (ClassesUtil.isPrimitive(object.getClass())) {
-                        strBuilder.append(object);
-                    } else {
-                        strBuilder.append(JSON.toJSONString(object).hashCode());
-                    }
-                });
-            } else {
-                strBuilder.append(sp);
-            }
-            return strBuilder.toString();
-        };
-    }
+  @Bean
+  @Override
+  public KeyGenerator keyGenerator() {
+    return (target, method, params) -> {
+      char sp = ':';
+      StringBuilder strBuilder = new StringBuilder(30);
+      // 类名
+      strBuilder.append(target.getClass().getSimpleName());
+      strBuilder.append(sp);
+      // 方法名
+      strBuilder.append(method.getName());
+      strBuilder.append(sp);
+      if (params.length > 0) {
+        // 参数值
+        Arrays.stream(params).forEach(object -> {
+          if (DataUtils.isEmpty(object)) {
+            strBuilder.append(Strings.EMPTY);
+          } else if (ClassesUtil.isPrimitive(object.getClass())) {
+            strBuilder.append(object);
+          } else {
+            strBuilder.append(JSON.toJSONString(object).hashCode());
+          }
+        });
+      } else {
+        strBuilder.append(sp);
+      }
+      return strBuilder.toString();
+    };
+  }
 
 }
