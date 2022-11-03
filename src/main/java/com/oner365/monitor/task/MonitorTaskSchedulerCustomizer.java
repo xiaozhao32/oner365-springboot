ecc0e93fa74f94131a1692b58e5804b86255ddc3
zhaoyong <xiaozhao32@hotmail.com>
2022-11-03 10:02:10 +0800

    update pom.xml org.apache.maven.plugins.version=3.3.0 org.hibernate.version=5.6.12.Final

---
+++ src/main/java/com/oner365/monitor/task/MonitorTaskSchedulerCustomizer.java
@@ -0,0 +1,33 @@
+package com.oner365.monitor.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.task.TaskSchedulerCustomizer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ErrorHandler;
+
+/**
+ * 定时任务 异常处理
+ * 
+ * @author zhaoyong
+ * 
+ */
+@Component
+public class MonitorTaskSchedulerCustomizer implements TaskSchedulerCustomizer {
+    
+    private final Logger logger = LoggerFactory.getLogger(MonitorTaskSchedulerCustomizer.class);
+
+    @Override
+    public void customize(ThreadPoolTaskScheduler taskScheduler) {
+        taskScheduler.setErrorHandler(new MonitorErrorHandler());
+    }
+    
+    private class MonitorErrorHandler implements ErrorHandler {
+        @Override
+        public void handleError(Throwable throwable) {
+            logger.error("ErrorHandler", throwable);
+        }
+    }
+
+}
