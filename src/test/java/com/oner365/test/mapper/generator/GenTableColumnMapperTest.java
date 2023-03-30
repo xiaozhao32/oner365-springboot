6d615672a34bf667602c1e87d7e2c7b7aaa894d4
zhaoyong <xiaozhao32@hotmail.com>
2023-03-30 16:40:07 +0800

    update junit

---
+++ src/test/java/com/oner365/test/mapper/generator/GenTableColumnMapperTest.java
@@ -0,0 +1,28 @@
+package com.oner365.test.mapper.generator;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.oner365.generator.entity.GenTableColumn;
+import com.oner365.generator.mapper.GenTableColumnMapper;
+import com.oner365.test.mapper.BaseMapperTest;
+
+/**
+ * Mapper 单元测试
+ * 
+ * @author zhaoyong
+ *
+ */
+public class GenTableColumnMapperTest extends BaseMapperTest {
+
+  @Test
+  void selectGenTableList() {
+    String tableName = "nt_sys_user";
+    List<GenTableColumn> list = getMapper(GenTableColumnMapper.class).selectDbTableColumnsByName(tableName);
+    logger.info("findList:{}", list.size());
+    Assertions.assertNotEquals(0, list.size());
+  }
+
+}
---
+++ src/test/java/com/oner365/test/mapper/generator/GenTableMapperTest.java
@@ -0,0 +1,54 @@
+package com.oner365.test.mapper.generator;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.oner365.generator.entity.GenTable;
+import com.oner365.generator.mapper.GenTableMapper;
+import com.oner365.test.mapper.BaseMapperTest;
+
+/**
+ * Mapper 单元测试
+ * 
+ * @author zhaoyong
+ *
+ */
+public class GenTableMapperTest extends BaseMapperTest {
+
+  @Test
+  void selectGenTableList() {
+    GenTable genTable = new GenTable();
+    genTable.setTableName("test");
+    List<GenTable> list = getMapper(GenTableMapper.class).selectGenTableList(genTable);
+    logger.info("findList:{}", list.size());
+    Assertions.assertNotEquals(0, list.size());
+  }
+
+  @Test
+  void selectDbTableList() {
+    GenTable genTable = new GenTable();
+    genTable.setTableName("user");
+    List<GenTable> list = getMapper(GenTableMapper.class).selectDbTableList(genTable);
+    logger.info("findList:{}", list.size());
+    Assertions.assertNotEquals(0, list.size());
+  }
+  
+  @Test
+  void selectDbTableListByNames() {
+    String[] tableNames = new String[] { "nt_sys_user" };
+    List<GenTable> list = getMapper(GenTableMapper.class).selectDbTableListByNames(tableNames);
+    logger.info("findList:{}", list.size());
+    Assertions.assertNotEquals(0, list.size());
+  }
+  
+  @Test
+  void selectGenTableByName() {
+    String tableName = "nt_test_date";
+    GenTable genTable = getMapper(GenTableMapper.class).selectGenTableByName(tableName);
+    logger.info("entity:{}", genTable);
+    Assertions.assertNotNull(genTable);
+  }
+  
+}
