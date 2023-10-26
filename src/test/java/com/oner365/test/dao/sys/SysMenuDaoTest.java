0c54671839d9f55cc521d0e9602bec48141c1c5e
zhaoyong <xiaozhao32@hotmail.com>
2023-10-26 18:43:50 +0800

    update pom.xml springboot-2.7.17

--- src/test/java/com/oner365/test/dao/sys/SysMenuDaoTest.java
+++ src/test/java/com/oner365/test/dao/sys/SysMenuDaoTest.java
@@ -1,8 +1,14 @@
 package com.oner365.test.dao.sys;
 
 import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import javax.annotation.Resource;
 
@@ -16,11 +22,16 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import com.alibaba.fastjson.JSON;
 import com.oner365.sys.dao.ISysMenuDao;
 import com.oner365.sys.dto.SysUserDto;
 import com.oner365.sys.entity.SysMenu;
 import com.oner365.test.dao.BaseDaoTest;
+import com.oner365.util.DateUtil;
 import com.oner365.util.excel.ExportExcelUtils;
 
 /**
@@ -52,6 +63,40 @@ class SysMenuDaoTest extends BaseDaoTest {
   }
   
   @Test
+  void testGetById() {
+    SysUserDto result = jdbcTemplate.queryForObject("SELECT * FROM nt_sys_user where id='1' ", 
+        BeanPropertyRowMapper.newInstance(SysUserDto.class));
+    logger.info("result:{}", JSON.toJSONString(result));
+  }
+  
+//  @Test  
+  void testInsert() {
+    String sql = "insert into nt_test_date(name, phone, price, description, test_date, status, create_time, update_time) values(?,?,?,?,?,?,?,?)";
+    String result = insertAutoIncrementId(sql,
+        new Object[] { "test", 999, 1.2d, "abc", new Date(), 1, DateUtil.getCurrentTime(), DateUtil.getCurrentTime() });
+    logger.info("result:{}", JSON.toJSONString(result));
+  }
+
+  public String insertAutoIncrementId(String sql, Object[] params) {
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    jdbcTemplate.update(new PreparedStatementCreator() {
+      @Override
+      public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        IntStream.range(0, params.length).forEach( i -> {
+          try {
+            ps.setObject(i+1, params[i]);
+          } catch (SQLException e) {
+            logger.error("insertAutoIncrementId error", e);
+          }
+        });
+        return ps;
+      }
+    }, keyHolder);
+    return keyHolder.getKey().toString();
+  }
+  
+//  @Test
   void testExportPage() {
     // 分页信息
     int size = 10000;
