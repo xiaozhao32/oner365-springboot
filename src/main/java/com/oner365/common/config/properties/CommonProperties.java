bd94e31c56daa8e9ca97e0f602126d925029fbb0
zhaoyong <xiaozhao32@hotmail.com>
2023-11-10 13:04:36 +0800

    elasticsearch设置密码 xpack.security.enabled: true

--- src/main/java/com/oner365/common/config/properties/CommonProperties.java
+++ src/main/java/com/oner365/common/config/properties/CommonProperties.java
@@ -18,12 +18,6 @@ public class CommonProperties {
   private boolean redisEnabled;
 
   /**
-   * elasticsearch地址
-   */
-  @Value("${spring.elasticsearch.uris}")
-  private String uris;
-  
-  /**
    * 项目名称
    */
   @Value("${spring.application.name}")
@@ -59,14 +53,6 @@ public class CommonProperties {
 
   public void setRedisEnabled(boolean redisEnabled) {
     this.redisEnabled = redisEnabled;
-  }
-
-  public String getUris() {
-    return uris;
-  }
-
-  public void setUris(String uris) {
-    this.uris = uris;
   }
 
   public String getServiceId() {
