1e57690ca0d685165a9a13661bb992a2e896ddde
zhaoyong <xiaozhao32@hotmail.com>
2024-08-07 13:04:19 +0800

    reactor httpclient test

--- src/test/java/com/oner365/test/util/ReactorUtilTest.java
+++ src/test/java/com/oner365/test/util/ReactorUtilTest.java
@@ -2,11 +2,8 @@ package com.oner365.test.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alibaba.fastjson.JSONObject;
 
@@ -20,28 +17,26 @@ import reactor.netty.http.client.HttpClient;
  * @author zhaoyong
  *
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReactorUtilTest extends BaseUtilsTest {
 
   @Test
   void testGetUrl() {
-    String url = "/actuator/health";
+    String uri = "http://localhost:8704/actuator/health";
     // GET
-    String result = HttpClient.create().baseUrl(url).get().responseContent().aggregate().asString().block();
+    String result = HttpClient.create().get().uri(uri).responseContent().aggregate().asString().block();
     logger.info("HttpClient GET : {}", result);
     Assertions.assertNotNull(result);
   }
 
   @Test
   void testPostUrl() {
-    String url = "/system/auth/login";
+    String uri = "http://localhost:8704/system/auth/login";
     JSONObject json = new JSONObject();
     json.put("userName", "admin");
     json.put("password", "1");
     // POST
     String result = HttpClient.create().headers(h -> h.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-        .baseUrl(url).post().send(ByteBufFlux.fromString(Mono.just(json.toJSONString()))).responseContent().aggregate()
+        .post().uri(uri).send(ByteBufFlux.fromString(Mono.just(json.toJSONString()))).responseContent().aggregate()
         .asString().block();
     logger.info("HttpClient POST : {}", result);
     Assertions.assertNotNull(result);
