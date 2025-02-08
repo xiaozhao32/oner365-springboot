a6b54b54d3ec7e78ea8a1bcff599ecf41b9367e8
zhaoyong <xiaozhao32@hotmail.com>
2025-02-08 11:05:13 +0800

    dockerfile 添加环境变量

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ EXPOSE 8704
 
 ENV TZ=Asia/Shanghai
 ENV PATH $PATH
+ENV SPRING_PROFILES_ACTIVE=dev
 
 ARG JAR_FILE
 ARG OUT_DIRECTORY
@@ -14,7 +15,8 @@ ARG OUT_DIRECTORY
 COPY ${OUT_DIRECTORY}/lib /root/oner365-springboot/lib/
 COPY ${OUT_DIRECTORY}/resources /root/oner365-springboot/resources/
 COPY ${JAR_FILE} /root/oner365-springboot/oner365-springboot-2.1.0.jar
-ENTRYPOINT ["java","-Dspring.profiles.active=dev","-jar","oner365-springboot-2.1.0.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}", \
+	"-jar", "/root/oner365-springboot/oner365-springboot-2.1.0.jar"]
 
 # 本地执行
 # mvn clean package -Dmaven.test.skip=true docker:build
--- src/test/java/com/oner365/test/util/JwtUtilsTest.java
+++ src/test/java/com/oner365/test/util/JwtUtilsTest.java
@@ -24,35 +24,46 @@ import com.oner365.data.commons.util.JwtUtils;
  *
  */
 class JwtUtilsTest extends BaseUtilsTest {
+  
+  private static final String KEY = "test";
+  
+  private static final Date expireTime = DateUtil.after(DateUtil.getDate(), 1440, Calendar.MINUTE);
 
   @Test
   void test() {
     JSONObject json = new JSONObject();
     json.put("userName", "test");
-    String key = "test";
-    Date time = DateUtil.after(DateUtil.getDate(), 1440, Calendar.MINUTE);
-    String token = JwtUtils.generateToken(json.toJSONString(), time, key);
-    logger.info("token:{}", token);
-    logger.info("解密token:{}", JwtUtils.getUsernameFromToken(token, key));
-    logger.info("validateToken:{}", JwtUtils.validateToken(token, key));
-    logger.info("base64 decode:");
+    String token = JwtUtils.generateToken(json.toJSONString(), expireTime, KEY);
+    // 打印token
+    logger.info("io.jsonwebtoken.Jwts token: {}", token);
+    Assertions.assertNotEquals(null, token);
+    // 解密token
+    String body = JwtUtils.getUsernameFromToken(token, KEY);
+    logger.info("io.jsonwebtoken.Jwts parse token: {}", body);
+    boolean isValid = JwtUtils.validateToken(token, KEY);
+    logger.info("io.jsonwebtoken.Jwts validateToken: {}", isValid);
+    Assertions.assertEquals(Boolean.TRUE, isValid);
+    
+    logger.info("io.jsonwebtoken.Jwts base64 decode:");
     Assertions.assertNotEquals(null, token);
     Arrays.stream(token.split("\\.")).filter(s -> s.startsWith("ey"))
-        .forEach(s -> logger.info("content:{}", new String(Base64.getDecoder().decode(s.trim()), Charset.defaultCharset())));
+        .forEach(s -> logger.info("io.jsonwebtoken.Jwts content:{}", new String(Base64.getDecoder().decode(s.trim()), Charset.defaultCharset())));
   }
 
   @Test
   void testJwtTools() {
     // 生成token
-    Date time = DateUtil.after(DateUtil.getDate(), 14400, Calendar.MINUTE);
     JSONObject json = JSON.parseObject(
         "{\"id\":\"CGWU5434122967LGB5E4\",\"tokenType\":\"login\",\"userName\":\"9D11AE753FC3824D79541C7A71EA8EA5\",\"deviceId\":\"1d96163d30db4ea97b7478e39f\"}");
-    String token = JwtTools.getToken(json.toJSONString(), time, "test");
+    String token = JwtTools.generateToken(json.toJSONString(), expireTime, KEY);
     // 打印token
-    logger.info("token: {}", token);
+    logger.info("com.auth0.jwt token: {}", token);
     Assertions.assertNotEquals(null, token);
     // 解密token
-    DecodedJWT jwt = JwtTools.decodeToken(token, "test");
-    logger.info("body: {}", Objects.requireNonNull(jwt).getClaim("body").asString());
+    DecodedJWT jwt = JwtTools.decodeToken(token, KEY);
+    logger.info("com.auth0.jwt parse token: {}", Objects.requireNonNull(jwt).getClaim("body").asString());
+    boolean isValid = JwtTools.validateToken(token, KEY);
+    logger.info("com.auth0.jwt validateToken: {}", isValid);
+    Assertions.assertEquals(Boolean.TRUE, isValid);
   }
 }
